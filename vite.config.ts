ed9674ce6b9a6ee50d7c74b3c5f95f238ec5019c
narendra <narendra.a@quiddityinfotech.com>
2025-10-22 12:44:03 +0530

    Clean rebase-free project restore

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
@@ -8,7 +8,7 @@ export default defineConfig({
     plugins: [react(), tailwindcss()],
     server: {
         port: 5173,
-        strictPort: true, // 🚨 If 5173 is busy, it will throw error instead of switching
+        strictPort: true, // 🚨 If 5173 is busy, it will throw an error instead of switching
     },
     build: {
         assetsDir: "assets",
